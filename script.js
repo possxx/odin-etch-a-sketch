5f74c7663fd921548f1da35dc76b408ec4803a3d
possxx <168581964+possxx@users.noreply.github.com>
2024-05-28 19:26:31 +0200

    Add gui to etch-a-sketch function

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const etchASketchContainer = document.querySelector("#eas-container");
 function createSketch(dimension) {
     for (let i = 0; i < dimension; i++) {
         const etchASketchRow = document.createElement("div");
+        etchASketchRow.classList.add("eas-row");
         etchASketchContainer.appendChild(etchASketchRow);
         for (let i =0; i < dimension; i++) {
             const etchASketchItem = document.createElement("div");
+            etchASketchItem.classList.add("eas-item");
             etchASketchRow.appendChild(etchASketchItem);
         };
     };
+    const etchASketchItems = document.querySelectorAll(".eas-item");
+    etchASketchItems.forEach((item) => {
+        item.style.width = `${etchASketchContainer.offsetWidth / dimension}px`;
+        item.style.height = `${etchASketchContainer.offsetWidth / dimension}px`;
+    });
 };
 
-createSketch(4);
+createSketch(3)
