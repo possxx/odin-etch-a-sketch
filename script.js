a6fd6be7d24b58fe14df0cc49855962130fa73dd
possxx <168581964+possxx@users.noreply.github.com>
2024-05-30 16:31:41 +0200

    Decrease brightness of rgb values on mouseenter

--- script.js
+++ script.js
@@ -33,7 +33,61 @@ function changeColor() {
     const itemColors = document.querySelectorAll(".eas-item");
     itemColors.forEach((item) => {
         item.addEventListener("mouseenter", () => {
-            item.style.backgroundColor = `rgb(${generateColor()})`;
+            if (item.style.backgroundColor == "") {
+                item.style.backgroundColor = `rgb(${generateColor()})`;  
+                item.setAttribute("id", "1")
+            } else {
+                // Take current background-color and revert it back to original if needed to decrement the rgb values by 10% for each time a mouseenter event is triggered
+                const currentColor = item.style.backgroundColor.slice(4, -1).split(",");
+                if (item.getAttribute("id") == "1") {
+                    let newColor = `rgb(${(currentColor[0] - (currentColor[0] / 10).toFixed(0))}, ${(currentColor[1] - (currentColor[1] / 10)).toFixed(0)}, ${(currentColor[2] - (currentColor[2] / 10)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.9");
+                } else if (item.getAttribute("id") == "0.9") {
+                    let initialColor = [(currentColor[0] / 9 * 10).toFixed(0), (currentColor[1] / 9 * 10).toFixed(0), (currentColor[2] / 9 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 2)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 2)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 2)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.8");
+                } else if (item.getAttribute("id") == "0.8") {
+                    let initialColor = [(currentColor[0] / 8 * 10).toFixed(0), (currentColor[1] / 8 * 10).toFixed(0), (currentColor[2] / 8 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 3)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 3)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 3)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.7");
+                } else if (item.getAttribute("id") == "0.7") {
+                    let initialColor = [(currentColor[0] / 7 * 10).toFixed(0), (currentColor[1] / 7 * 10).toFixed(0), (currentColor[2] / 7 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 4)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 4)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 4)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.6");
+                } else if (item.getAttribute("id") == "0.6") {
+                    let initialColor = [(currentColor[0] / 6 * 10).toFixed(0), (currentColor[1] / 6 * 10).toFixed(0), (currentColor[2] / 6 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 5)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 5)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 5)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.5");
+                } else if (item.getAttribute("id") == "0.5") {
+                    let initialColor = [(currentColor[0] / 5 * 10).toFixed(0), (currentColor[1] / 5 * 10).toFixed(0), (currentColor[2] / 5 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 6)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 6)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 6)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.4");
+                } else if (item.getAttribute("id") == "0.4") {
+                    let initialColor = [(currentColor[0] / 4 * 10).toFixed(0), (currentColor[1] / 4 * 10).toFixed(0), (currentColor[2] / 4 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 7)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 7)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 7)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.3");
+                } else if (item.getAttribute("id") == "0.3") {
+                    let initialColor = [(currentColor[0] / 3 * 10).toFixed(0), (currentColor[1] / 3 * 10).toFixed(0), (currentColor[2] / 3 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 8)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 8)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 8)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.2");
+                } else if (item.getAttribute("id") == "0.2") {
+                    let initialColor = [(currentColor[0] / 2 * 10).toFixed(0), (currentColor[1] / 2 * 10).toFixed(0), (currentColor[2] / 2 * 10).toFixed(0)];
+                    let newColor = `rgb(${(initialColor[0] - ((initialColor[0] / 10) * 9)).toFixed(0)}, ${(initialColor[1] - ((initialColor[1] / 10) * 9)).toFixed(0)}, ${(initialColor[2] - ((initialColor[2] / 10) * 9)).toFixed(0)})`;
+                    item.style.backgroundColor = newColor;
+                    item.setAttribute("id", "0.1");
+                } else {
+                    item.style.backgroundColor = "rgb(0, 0, 0)";
+                    item.setAttribute("id", "0");
+                }
+            }
         })
     })
 }
